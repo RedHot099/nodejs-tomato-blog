2a5b004e780974342318d9e2ecec4f3ff237d6e7
Jakub <jakeic@st.amu.edu.pl>
2022-05-20 19:05:40 +0200

    v1.0

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const path = require('path');
 
 const port = process.env.port || 5000;
 
-app.use(express.static(__dirname + '/HTML'));
+app.use(express.static(__dirname + '/resources'));
 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/index.html'));
+    res.sendFile(path.join(__dirname + '/resources/index.html'));
 })
 
 app.get('/pomidory-paprykowe.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/pomidory-paprykowe.html'));
+    res.sendFile(path.join(__dirname + '/resources/pomidory-paprykowe.html'));
 })
 app.get('/pomidory-czekoladowe.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/pomidory-czekoladowe.html'));
+    res.sendFile(path.join(__dirname + '/resources/pomidory-czekoladowe.html'));
 })
 app.get('/czarne-pomidory.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/czarne-pomidory.html'));
+    res.sendFile(path.join(__dirname + '/resources/czarne-pomidory.html'));
 })
 app.get('/pomidory-tygrysie.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/pomidory-tygrysie.html'));
+    res.sendFile(path.join(__dirname + '/resources/pomidory-tygrysie.html'));
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
